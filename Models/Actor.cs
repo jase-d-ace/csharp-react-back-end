a70d27729194785ee50faae20b0114c761cc6704
Jason Andrada <jpa323@nyu.edu>
2019-10-28 14:16:10 -0400

    rename table columns in model

--- Models/Actor.cs
+++ Models/Actor.cs
@@ -2,7 +2,7 @@ namespace helloapi.Models {
   public class Actor {
     public int id { get; set; }
     public string name { get; set; }
-    public int MovieId { get; set; }
+    public int movie_id { get; set; }
     public Movie Movie { get; set; }
   }
 }
