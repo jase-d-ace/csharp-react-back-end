d4ba875c8f23244ac992209b98ad5e2703494746
Jason Andrada <jpa323@nyu.edu>
2019-10-28 14:19:35 -0400

    rename table columns in model 2

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,6 +1,6 @@
 namespace helloapi.Models {
   public class Todo {
-    public long Id { get; set; }
+    public long id { get; set; }
     public string text { get; set; }
     public bool complete { get; set; }
   }
